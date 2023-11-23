2d677002ddb6a23d889b9bd440927376e582b5d7
Dendy-D <daniilvasilenko2@gmail.com>
2023-11-24 01:21:01 +0300

    add some tasks

--- commonTasks.js
+++ commonTasks.js
@@ -17,7 +17,7 @@ function reverseString(str) {
   return arr.join('');
 }
 
-// remove duplicates (in place)
+// remove duplicates in the array (in place)
 
 function removeDuplicates(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -31,7 +31,7 @@ function removeDuplicates(arr) {
   return arr;
 }
 
-// swap elements in an array
+// swap elements in an array (in place)
 
 function swapTwoElements(arr, index1, index2) {
   const temp = arr[index1];
@@ -40,7 +40,7 @@ function swapTwoElements(arr, index1, index2) {
   return arr;
 }
 
-// remove all occurrences in an array
+// remove all occurrences in an array (in place)
 
 function removeAllOccurrences(arr, target) {
   for (let i = 0; i < arr.length; i++) {
@@ -51,4 +51,91 @@ function removeAllOccurrences(arr, target) {
   return arr;
 }
 
-  
+// get the object with frequecy of words occurencies in an array 
+
+function wordFrequencyCounter(arr) {
+  const map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i])) {
+      map.set(arr[i], map.get(arr[i]) + 1)
+    } else {
+      map.set(arr[i], 1);
+    }
+  }
+  return map;
+}
+
+
+// group objects by their name
+
+function groupObjects(arr) {
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i].name)) {
+      map.set(arr[i].name, [...map.get(arr[i].name), arr[i]])
+    } else {
+      map.set(arr[i].name, [arr[i]]);
+    }
+  }
+
+  return map;
+}
+
+// group anagrams
+
+function anagramGroups(arr) {
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let sortedString = [...arr[i]].sort().join('');
+    if (map.has(sortedString)) {
+      map.set(sortedString, [...map.get(sortedString), arr[i]]);
+    } else {
+      map.set(sortedString, [arr[i]])
+    }
+  }
+
+  return map;
+}
+
+// find the longest consecutive sequence of numbers in an array
+// O(n log n) approach (because I sorted my array here, but I think it's possible to avoid it...)
+
+function getTheLongestConsecutiveSequence(arr) {
+  const sortedArr = arr.sort((a, b) => a - b);
+  let result = 0;
+  let count = 0;
+
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i + 1] === sortedArr[i] + 1) {
+      count++;
+    } else {
+      result = result > count ? result : count; 
+      count = 0;
+    }
+  }
+
+  return result;
+}
+
+// find the longest consecutive sequence of numbers in an array
+// O(n) approach
+
+function getTheLongestConsecutiveSequence2(arr) {
+  const set = new Set(arr);
+  let longestSequence = 0;
+  let count = 0;
+
+  for (let num of set) {
+    let elemOfSequence = num;
+    while(set.has(elemOfSequence + 1)) {
+      count++;
+      elemOfSequence++;
+    }
+    longestSequence = longestSequence > count ? longestSequence : count;
+    count = 0;
+  }
+
+  return longestSequence;
+}
---
+++ promises.js
@@ -0,0 +1,12 @@
+function PromiseAll(arrOfPromises) {
+  const result = [];
+
+  for (let i = 0; i < arrOfPromises.length; i++) {
+    new Promise((resolve, reject) => {
+      
+    })
+  }
+
+  return result;
+}
+
